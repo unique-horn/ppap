0c6411f0646ac3aae0cb4e3df2221b7737260145
Abhinav Tushar <abhinav.tushar.vs@gmail.com>
2016-11-19 16:04:49 -0500

    Basic mask

--- ppap/layers/ppmask.py
+++ ppap/layers/ppmask.py
@@ -2,7 +2,47 @@
 PPN based adaptive masking layers
 """
 
-import numpy as np
-from keras import backend as K
-from keras import initializations
 from keras.engine.topology import Layer
+
+from .. import generators
+
+
+class PPAdaptiveMask(Layer):
+    """
+    Return a generated mask
+    To be used in conjugation with keras.layers.Merge and 'mul' mode
+    """
+
+    def __init__(self, output_shape, layer_sizes, **kwargs):
+        """
+        """
+
+        self.output_shape = output_shape
+        self.layer_sizes = layer_sizes
+        self.gen = generators.FFMatrixGen(output_shape=output_shape,
+                                          layer_sizes=layer_sizes)
+
+        super().__init__(**kwargs)
+
+    def build(self, input_shape):
+        """
+        """
+
+        self.mask = self.gen.output
+
+        self.trainable_weights = self.gen.weights
+        self.non_trainable_weights = [self.mask]
+
+        self.built = True
+
+    def get_output_shape_for(self, input_shape):
+        """
+        """
+
+        return self.output_shape
+
+    def call(self, x, mask=None):
+        """
+        """
+
+        return self.mask
