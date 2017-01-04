578e7672ea5ce620c7e679b76701508ba913760f
Abhinav Tushar <abhinav.tushar.vs@gmail.com>
2017-01-04 01:42:44 -0500

    Strip down setuptools files

--- setup.py
+++ setup.py
@@ -1,14 +1,7 @@
-#!/usr/bin/env python
-
 from setuptools import find_packages, setup
 
 with open("README.rst") as readme_file:
     readme = readme_file.read()
-
-with open("requirements.txt") as requirements_file:
-    requirements = requirements_file.read().splitlines()
-
-install_requires = [x.strip() for x in requirements]
 
 project_url = "https://github.com/"
 project_url += "unique-horn/ppap"
@@ -21,16 +14,11 @@ setup(
     author="Unique Horn",
     author_email="",
     url=project_url,
-    include_package_data=True,
-    install_requires=install_requires,
-    license="GPL v3",
+    install_requires=["keras>=1.1.0", "noise"],
     keywords="",
     packages=find_packages(exclude=["docs", "tests*"]),
-    setup_requires=["pytest-runner"],
-    tests_require=["pytest"],
     classifiers=(
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Natural Language :: English",
         "Programming Language :: Python"
-    ),
-    zip_safe=False)
+    ))
